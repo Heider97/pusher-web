21cd31924e4a285cce768428e979eea610c307a1
BexHeider <heider.zapa@bexsoluciones.com>
2025-01-29 19:49:37 -0500

    exclude webhook to pusher

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'paystack/webhook'
+        ]);
         $middleware->trustProxies(
             at: '*',
             headers: Request::HEADER_X_FORWARDED_AWS_ELB,
