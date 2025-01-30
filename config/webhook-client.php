85c237091870addc2e038c221ee2480d6a1b3b9b
BexHeider <heider.zapa@bexsoluciones.com>
2025-01-29 19:58:29 -0500

    exclude webhook to pusher

--- config/webhook-client.php
+++ config/webhook-client.php
@@ -20,7 +20,7 @@ return [
             /*
              * The name of the header containing the signature.
              */
-            'signature_header_name' => 'Signature',
+            'signature_header_name' => '65a75e20824deceb4fe6',
 
             /*
              *  This class will verify that the content of the signature header is valid.
